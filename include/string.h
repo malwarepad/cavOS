fee9d2b8de12001dd34e0972ef8411b7872a4d3c
Panagiotis <contact@malwarepad.com>
2021-10-14 17:22:30 +0300

    almost there to adding arguments

--- include/string.h
+++ include/string.h
@@ -7,5 +7,7 @@ uint16 strlength(string ch);
 uint8 strEql(string ch1,string ch2);
 uint8 cmdEql(string ch1, string ch2);
 uint8 cmdLength(string ch);
+uint8 whereSpace1(string ch1);
+uint8 searchArg1(string ch1, string ch2);
 
 #endif
