1263056684d554d5d574a7574eac7f1d18362467
Panagiotis Kalos <panagiotiskkalos@gmail.com>
2021-10-20 23:07:08 +0300

    Chenged the whole directory structure

---
+++ src/entry/kernel.c
@@ -0,0 +1,13 @@
+#include "../../include/kb.h"
+#include "../../include/isr.h"
+#include "../../include/idt.h"
+#include "../../include/util.h"
+#include "../../include/shell.h"
+
+int kmain()
+{
+	isr_install();
+	clearScreen();
+	printf("Welcome to cavOS! The OS that reminds you of how good computers \nwere back then.. Anyway, just execute any command you want\n'help' is your friend :)\n\n");
+  	launch_shell(0);
+}
