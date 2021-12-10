b5121405cc248fc8873e80c30210b1368885ffae
Panagiotis <contact@malwarepad.com>
2021-12-10 13:50:02 +0200

    Added more extensible math support (future mathf headers) and fixed more bugs.

---
+++ include/math.h
@@ -0,0 +1,12 @@
+#ifndef MATH_H
+#define MATH_H
+
+#include "types.h"
+
+uint8 math_add(uint8 num1, uint8 num2);
+uint8 math_remove(uint8 num1, uint8 num2);
+uint8 math_epi(uint8 num1, uint8 num2);
+uint8 math_dia(uint8 num1, uint8 num2);
+uint8 math_get_current_equation(string str);
+
+#endif
