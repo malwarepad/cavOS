347259642aac74bc228e2c955a7b12e3f0d8b8b0
Panagiotis Kalos <panagiotiskkalos@gmail.com>
2021-12-14 19:08:20 +0200

    Created the base of creating a basic calculator

--- include/math.h
+++ include/math.h
@@ -8,5 +8,8 @@ uint8 math_remove(uint8 num1, uint8 num2);
 uint8 math_epi(uint8 num1, uint8 num2);
 uint8 math_dia(uint8 num1, uint8 num2);
 uint8 math_get_current_equation(string str);
+uint8 math_check_possible_equations(string str);
+uint8 math_check_possible_equations_advanced(string str, uint8 type);
+uint8 math_check_previous_equations(string str, uint8 position);
 
 #endif
--- src/entry/kernel.c
+++ src/entry/kernel.c
@@ -4,10 +4,13 @@
 #include "../../include/util.h"
 #include "../../include/shell.h"
 
+#include "../../include/math.h"
+
 int kmain()
 {
 	isr_install();
 	clearScreen();
 	printf("Welcome to cavOS! The OS that reminds you of how good computers \nwere back then.. Anyway, just execute any command you want\n'help' is your friend :)\n\n");
+	printf("%d", math_check_previous_equations("12+12+12", 2));
 	launch_shell(0);
 }
