40bf9b63a26a83287aa98462e045b658be3716f8
Panagiotis <contact@malwarepad.com>
2022-03-18 16:51:13 +0200

    Added basic support for GUI. Project slowly inverting scope.

--- src/entry/kernel.c
+++ src/entry/kernel.c
@@ -1,18 +1,16 @@
 #include "../../include/kb.h"
+#include "../../include/vga.h"
 #include "../../include/isr.h"
 #include "../../include/idt.h"
 #include "../../include/util.h"
 #include "../../include/shell.h"
+#include "../../include/multiboot.h"
 
-#include "../../include/coding_functions.h"
-
-char* commands[4][512];
-uint8 count = 0;
-
-int kmain()
+int kmain(uint32 magic, multiboot_info_t *mbi)
 {
+    (void) magic; /* Silence compiler warning since magic isn't used */
 	isr_install();
 	clearScreen();
 	printf("Welcome to cavOS! The OS that reminds you of how good computers \nwere back then.. Anyway, just execute any command you want\n'help' is your friend :)\n\n");
-    launch_shell(0);
+	launch_shell(0);
 }
