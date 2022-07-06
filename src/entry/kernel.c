6b08e501524597203bc3ff9c22538293c4d5b282
Panagiotis <contact@malwarepad.com>
2022-07-06 09:01:53 +0300

    Implement a very early stage disk and fat driver

--- src/entry/kernel.c
+++ src/entry/kernel.c
@@ -9,6 +9,10 @@
 
 #include "../wm/software/terminal.h"
 
+#include "../../include/disk.h"
+#include "../../include/fat.h"
+#include "../../include/x86.h"
+
 // Kernel entry file
 // Copyright (C) 2022 Panagiotis
 
@@ -21,4 +25,32 @@ int kmain(uint32 magic, multiboot_info_t *mbi)
     changeBg(0, 0, 0);
 	printf(mbi, "Welcome to cavOS! The OS that reminds you of how good computers \nwere back then.. Anyway, just execute any command you want\n'help' is your friend :)\n\nNote that this program comes with ABSOLUTELY NO WARRANTY.\n\n");
 	launch_shell(0, mbi);
+
+    /*uint32* target;
+    read_sectors_ATA_PIO(target, 0x0, 2);
+
+    for (int i = 0; i < 128; i++) {
+        printf(mbi, "%d", target[i] & 0xFF);
+        printf(mbi, "%d", (target[i] >> 8) & 0xFF);
+        printf(mbi, " ");
+    }*/
+
+
+    /*if (!FAT_Initialize(&disk)) {
+        printf(mbi, "error \n");
+        while (1==1) {}
+    }*/
+
+    /*FAT_File* fd = FAT_Open(&disk, "/");
+    FAT_DirectoryEntry entry;
+    while (FAT_ReadEntry(&disk, fd, &entry)) {
+        printf(mbi, "");
+        for (int i = 0; i < 11; i++) {
+            printf(mbi, "%c", entry.Name[i]);
+        }
+        printf(mbi, "\n");
+    }
+    FAT_Close(fd);*/
+
+    while (1==1) {}
 }
