6a94ace94c23de2402890879912c973e8787e8cf
Panagiotis <contact@malwarepad.com>
2023-10-04 20:21:46 +0000

    Add "random" function

--- src/utilities/util.c
+++ src/utilities/util.c
@@ -156,6 +156,15 @@ int memcmp(const void *aptr, const void *bptr, size_t size) {
   return 0;
 }
 
+static unsigned long int next = 1;
+
+int rand(void) {
+  next = next * 1103515245 + 12345;
+  return (unsigned int)(next / 65536) % 32768;
+}
+
+void srand(unsigned int seed) { next = seed; }
+
 // int   count;
 /*void *malloc(int nbytes) {
   // count += nbytes;
