3d8b8efc9173b4417cefbd849d1fb9d756379cdd
Panagiotis <contact@malwarepad.com>
2023-10-22 21:37:13 +0000

    Initial support for multiple drives fat32.c needs to allow multiple FAT32* structures by implicitly passing them to functions instead of using a global one declared at compile time

--- include/disk.h
+++ include/disk.h
@@ -4,15 +4,29 @@
 #include "types.h"
 #include <stdint.h>
 
+#define SECTOR_SIZE 512
 #define getDiskBytes read_sectors_ATA_PIO
-
-#define SECTOR_SIZE 512 // we assume each sector is exactly 512 bytes long
 
 #ifndef DISK_H
 #define DISK_H
 
-// void getDiskBytes(unsigned char *target, uint32_t LBA, uint8_t sector_count);
-void putDiskBytes(const unsigned char *source, uint32_t LBA,
-                  uint8_t sector_count);
+#define MBR_PARTITION_1 0x01BE
+#define MBR_PARTITION_2 0x01CE
+#define MBR_PARTITION_3 0x01DE
+#define MBR_PARTITION_4 0x01EE
+
+#define MBR_BOOTABLE 0x80
+#define MBR_REGULAR 0x00
+
+typedef struct {
+  uint8_t  status;
+  uint8_t  chs_first_sector[3];
+  uint8_t  type;
+  uint8_t  chs_last_sector[3];
+  uint32_t lba_first_sector;
+  uint32_t sector_count;
+} mbr_partition;
+
+void openDisk(uint32_t disk, uint8_t partition, mbr_partition *out);
 
 #endif
--- src/drivers/disk.c
+++ src/drivers/disk.c
@@ -1,43 +1,15 @@
 #include "../../include/disk.h"
 
-// Ata pio driver
+// Multiple disk handler
 // Copyright (C) 2022 Panagiotis
 
-// void getDiskBytes(unsigned char *target, uint32_t LBA, uint8_t sector_count)
-// { uint32_t *read = (uint32_t *)malloc((sector_count * SECTOR_SIZE) / 4);
-// read_sectors_ATA_PIO(read, LBA, sector_count);
+uint16_t mbr_partition_indexes[] = {MBR_PARTITION_1, MBR_PARTITION_2,
+                                    MBR_PARTITION_3, MBR_PARTITION_4};
 
-// uint16_t *target = (uint16_t *)target_address;
-
-// int writingCurr = 0;
-// for (int i = 0; i < ((sector_count * SECTOR_SIZE) / 4); i++) {
-//   target[writingCurr] = read[i] & 0xFF;
-//   target[writingCurr + 1] = (read[i] >> 8) & 0xFF;
-//   target[writingCurr + 2] = (read[i] >> 16) & 0xFF;
-//   target[writingCurr + 3] = (read[i] >> 24) & 0xFF;
-//   writingCurr += 4;
-// }
-
-// read_sectors_ATA_PIO(target, LBA, sector_count);
-
-// free(read);
-// }
-
-void putDiskBytes(const unsigned char *source, uint32_t LBA,
-                  uint8_t sector_count) {
-  uint32_t write_buffer[SECTOR_SIZE / 4]; // Create a buffer for 32-bit writes
-
-  int writingCurr = 0;
-  for (int i = 0; i < sector_count; i++) {
-    for (int j = 0; j < SECTOR_SIZE / 4; j++) {
-      // Combine 4 bytes into a 32-bit word
-      write_buffer[j] = ((uint32_t)source[writingCurr]) |
-                        (((uint32_t)source[writingCurr + 1]) << 8) |
-                        (((uint32_t)source[writingCurr + 2]) << 16) |
-                        (((uint32_t)source[writingCurr + 3]) << 24);
-      writingCurr += 4;
-    }
-    // Write the 32-bit buffer to the disk
-    write_sectors_ATA_PIO(LBA + i, 1, write_buffer);
-  }
+void openDisk(uint32_t disk, uint8_t partition, mbr_partition *out) {
+  disk = disk; // future plans
+  uint8_t *rawArr = (uint8_t *)malloc(SECTOR_SIZE);
+  read_sectors_ATA_PIO(rawArr, 0x0, 1);
+  *out = *(mbr_partition *)(&rawArr[mbr_partition_indexes[partition]]);
+  free(rawArr);
 }
