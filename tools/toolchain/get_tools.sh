0f5a8b3e9655065d98ffe5aaab8249018f78814f
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-08-09 08:48:25 +0300

    add i386 (32-bit) binutils and gcc compilation step. fix bug when autotools didn't get detected if they are already in system

--- tools/toolchain/get_tools.sh
+++ tools/toolchain/get_tools.sh
@@ -1,78 +1,14 @@
 #!/usr/bin/env bash
-set -x # show cmds
-set -e # fail globally
 
 # Know where we at :p
 SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+export SCRIPTPATH=$(dirname "$SCRIPT")
 cd "${SCRIPTPATH}"
 
-export PREFIX="$HOME/opt/cross"
-export TARGET=x86_64-cavos
-export SYSROOT="$SCRIPTPATH/../../target"
-export PATH="$PREFIX/bin:$PATH"
+targets=("i386-cavos" "x86_64-cavos")
 
-mkdir -p "$PREFIX"
+for i in "${targets[@]}"; do
+    export TARGET=$i
 
-# Ensure autotools
-if ! test -f "$HOME/opt/autotools_gcc/bin/automake"; then
-	chmod +x get_autotools.sh
-	./get_autotools.sh "1.15.1" "2.69" "$HOME/opt/autotools_gcc"
-	export PATH=$HOME/opt/autotools_gcc/bin:$PATH
-fi
-
-mkdir -p temporarydir
-cd temporarydir
-
-# Tarballs
-wget -nc https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.gz
-tar xpvf binutils-*.tar.gz
-wget -nc https://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz
-tar xpvf gcc-*.tar.gz
-
-# Binutils
-cd binutils-2.38
-
-# Binutils: Pre-build
-patch -p1 <"${SCRIPTPATH}/../../patches/binutils.diff"
-cd ld/
-automake
-cd ../
-
-mkdir -p build
-cd build
-../configure --target="$TARGET" --prefix="$PREFIX" --with-sysroot="$SYSROOT" --enable-shared
-make -j$(nproc)
-make install
-cd ../../
-
-# GCC
-cd gcc-11.4.0
-
-# GCC: Pre-build
-patch -p1 <"${SCRIPTPATH}/../../patches/gcc.diff"
-cd libstdc++-v3/
-autoconf
-cd ../
-
-./contrib/download_prerequisites
-
-mkdir -p build
-cd build
-../configure --target="$TARGET" --prefix="$PREFIX" --enable-languages=c,c++ --with-sysroot="$SYSROOT" --enable-shared
-make all-gcc -j$(nproc)
-make all-target-libgcc -j$(nproc)
-make install-gcc
-make install-target-libgcc
-
-# Building musl alone is essential before libstdc++ is built & installed
-chmod +x "${SCRIPTPATH}/../../src/libs/musl/build.sh"
-"${SCRIPTPATH}/../../src/libs/musl/build.sh" --noreplace
-
-make all-target-libstdc++-v3 -j$(nproc)
-make install-target-libstdc++-v3
-
-cd ../../
-
-cd ../
-rm -rf temporarydir
+    bash make_tool.sh
+done
---
+++ tools/toolchain/make_tool.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+set -x # show cmds
+set -e # fail globally
+
+export PREFIX="$HOME/opt/cross"
+#export TARGET=x86_64-cavos
+export SYSROOT="$SCRIPTPATH/../../target"
+export PATH="$PREFIX/bin:$PATH"
+
+mkdir -p "$PREFIX"
+
+# Ensure autotools
+if ! test -f "$HOME/opt/autotools_gcc/bin/automake"; then
+	chmod +x get_autotools.sh
+	./get_autotools.sh "1.15.1" "2.69" "$HOME/opt/autotools_gcc"
+fi
+export PATH=$HOME/opt/autotools_gcc/bin:$PATH
+
+mkdir -p temporarydir
+cd temporarydir
+
+# Tarballs
+wget -nc https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.gz
+tar xpvf binutils-*.tar.gz
+wget -nc https://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz
+tar xpvf gcc-*.tar.gz
+
+# Binutils
+cd binutils-2.38
+
+# Binutils: Pre-build
+patch -p1 <"${SCRIPTPATH}/../../patches/binutils.diff"
+cd ld/
+automake
+cd ../
+
+mkdir -p build
+cd build
+../configure --target="$TARGET" --prefix="$PREFIX" --with-sysroot="$SYSROOT" --enable-shared
+make -j$(nproc)
+make install
+cd ../../
+
+# GCC
+cd gcc-11.4.0
+
+# GCC: Pre-build
+patch -p1 <"${SCRIPTPATH}/../../patches/gcc.diff"
+cd libstdc++-v3/
+autoconf
+cd ../
+
+./contrib/download_prerequisites
+
+mkdir -p build
+cd build
+../configure --target="$TARGET" --prefix="$PREFIX" --enable-languages=c,c++ --with-sysroot="$SYSROOT" --enable-shared
+make all-gcc -j$(nproc)
+make all-target-libgcc -j$(nproc)
+make install-gcc
+make install-target-libgcc
+
+# Only build libc for x86_64, since user-land is 64-bit only for now
+if [ "$TARGET" = "x86_64-cavos" ]; then
+	# Building musl alone is essential before libstdc++ is built & installed
+	chmod +x "${SCRIPTPATH}/../../src/libs/musl/build.sh"
+	"${SCRIPTPATH}/../../src/libs/musl/build.sh" --noreplace
+
+	make all-target-libstdc++-v3 -j$(nproc)
+	make install-target-libstdc++-v3
+fi
+
+cd ../../
+
+cd ../
+rm -rf temporarydir
