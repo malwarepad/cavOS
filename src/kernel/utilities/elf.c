1fbff5dc9ceb8d10c2e34c3803539c743a0ca75d
Panagiotis <contact@malwarepad.com>
2024-11-23 20:21:56 +0200

    Fix default fd flags

--- src/kernel/utilities/elf.c
+++ src/kernel/utilities/elf.c
@@ -230,9 +230,9 @@ Task *elfExecute(char *filepath, uint32_t argc, char **argv, uint32_t envc,
   // fsUserOpenSpecial(a, "/dev/fb0", target, -1, &fb0);
   // fsUserOpenSpecial(a, "/dev/tty", target, -1, &stdio);
 
-  int stdin = fsUserOpen(target, "/dev/stdin", O_RDWR, 0);
-  int stdout = fsUserOpen(target, "/dev/stdout", O_RDWR, 0);
-  int stderr = fsUserOpen(target, "/dev/stderr", O_RDWR, 0);
+  int stdin = fsUserOpen(target, "/dev/stdin", O_RDWR | O_APPEND, 0);
+  int stdout = fsUserOpen(target, "/dev/stdout", O_RDWR | O_APPEND, 0);
+  int stderr = fsUserOpen(target, "/dev/stderr", O_RDWR | O_APPEND, 0);
 
   if (stdin < 0 || stdout < 0 || stderr < 0) {
     debugf("[elf] Couldn't establish basic IO!\n");
