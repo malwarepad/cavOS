503dca47d9cf57c12aaa2109b6e7edefbe48a672
Panagiotis <contact@malwarepad.com>
2025-03-22 12:33:37 +0200

    Fake return on signal syscalls so xorg stops bothering us

--- src/kernel/syscalls/linux/syscalls_sig.c
+++ src/kernel/syscalls/linux/syscalls_sig.c
@@ -1,20 +1,22 @@
 #include <linux.h>
 #include <syscalls.h>
 
-/*#define SYSCALL_RT_SIGACTION 13
+#define SYSCALL_RT_SIGACTION 13
 static size_t syscallRtSigaction(int sig, const struct sigaction *act,
                                  struct sigaction *oact, size_t sigsetsize) {
-  return ERR(ENOSYS);
+  dbgSysStubf("todo signals");
+  return 0;
 }
 
 #define SYSCALL_RT_SIGPROCMASK 14
 static size_t syscallRtSigprocmask(int how, __sigset_t *nset, __sigset_t *oset,
                                    size_t sigsetsize) {
-  return ERR(ENOSYS);
-}*/
+  dbgSysStubf("todo signals");
+  return 0;
+}
 
 void syscallRegSig() {
   // a
-  // registerSyscall(SYSCALL_RT_SIGACTION, syscallRtSigaction);
-  // registerSyscall(SYSCALL_RT_SIGPROCMASK, syscallRtSigprocmask);
+  registerSyscall(SYSCALL_RT_SIGACTION, syscallRtSigaction);
+  registerSyscall(SYSCALL_RT_SIGPROCMASK, syscallRtSigprocmask);
 }
