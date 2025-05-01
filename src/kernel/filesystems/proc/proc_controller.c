7e6758d15fbcc36a78de69f307116cc387f3be54
Panagiotis <contact@malwarepad.com>
2025-05-01 21:27:48 +0300

    Dirty support for procfs stat. Useful for debugging

--- src/kernel/filesystems/proc/proc_controller.c
+++ src/kernel/filesystems/proc/proc_controller.c
@@ -16,6 +16,15 @@
 
 Fakefs rootProc = {0};
 
+enum { PROC_INTERNAL_CMDLINE = 99, PROC_INTERNAL_STAT };
+typedef struct UserspaceProc {
+  uint64_t pid;
+  int      opcode;
+
+  Spinlock LOCK_PROP;
+  int      timesOpened;
+} UserspaceProc;
+
 size_t meminfoRead(OpenFile *fd, uint8_t *out, size_t limit) {
   char   buff[1024] = {0};
   size_t allocated = physical.allocatedSizeInBlocks * BLOCK_SIZE / 1024;
@@ -56,14 +65,113 @@ size_t uptimeRead(OpenFile *fd, uint8_t *out, size_t limit) {
 VfsHandlers handleUptime = {
     .read = uptimeRead, .seek = fsSimpleSeek, .stat = fakefsFstat};
 
+char procDetermineState(Task *task) {
+  if (task->state == TASK_STATE_READY)
+    return 'R';
+  if (task->state == TASK_STATE_DEAD || task->state == TASK_STATE_CREATED)
+    return 'X';
+  if (signalsRevivableState(task->state))
+    return 'S';
+  return 'D';
+}
+
 size_t sRead(OpenFile *fd, uint8_t *out, size_t limit) {
-  int   pid = (int)(size_t)fd->dir;
-  Task *target = taskGet(pid);
+  UserspaceProc *uproc = fd->dir;
+  Task          *target = taskGet(uproc->pid);
   assert(target);
-  size_t toCopy = MIN(target->cmdlineLen - fd->pointer, limit);
-  memcpy(out, &target->cmdline[fd->pointer], toCopy);
-  fd->pointer += toCopy;
-  return toCopy;
+
+  size_t ret = -1;
+  switch (uproc->opcode) {
+  case PROC_INTERNAL_CMDLINE: {
+    size_t toCopy = MIN(target->cmdlineLen - fd->pointer, limit);
+    memcpy(out, &target->cmdline[fd->pointer], toCopy);
+    fd->pointer += toCopy;
+    ret = toCopy;
+    break;
+  }
+  case PROC_INTERNAL_STAT: {
+    int32_t     pid = target->id;
+    const char *comm = "todo";
+    char        state = procDetermineState(target);
+
+    int32_t  ppid = target->parent->id;
+    int32_t  pgrp = target->pgid;
+    int32_t  session = 1234;
+    int32_t  tty_nr = 0;
+    int32_t  tpgid = -1;
+    uint32_t flags = 0;
+
+    uint64_t minflt = 10;
+    uint64_t cminflt = 20;
+    uint64_t majflt = 1;
+    uint64_t cmajflt = 2;
+
+    uint64_t utime = 100;
+    uint64_t stime = 200;
+    int64_t  cutime = 10;
+    int64_t  cstime = 5;
+
+    int64_t priority = 20;
+    int64_t nice = 0;
+    int64_t num_threads = 1;
+    int64_t itrealvalue = 0;
+
+    uint64_t starttime = 123456789;
+
+    uint64_t vsize = 12345678;
+    int64_t  rss = 500;
+    uint64_t rsslim = 0xffffffffffffffffULL;
+
+    uint64_t startcode = 0x400000;
+    uint64_t endcode = 0x401000;
+    uint64_t startstack = 0x7fff0000;
+    uint64_t kstkesp = 0x7fff1000;
+    uint64_t kstkeip = 0x400123;
+
+    uint64_t signal = 0, blocked = 0, sigignore = 0, sigcatch = 0;
+    uint64_t wchan = 0, nswap = 0, cnswap = 0;
+
+    int32_t  exit_signal = 17;
+    int32_t  processor = 0;
+    uint32_t rt_priority = 0, policy = 0;
+
+    uint64_t delayacct_blkio_ticks = 0;
+    uint64_t guest_time = 0, cguest_time = 0;
+
+    uint64_t start_data = 0x601000;
+    uint64_t end_data = 0x602000;
+    uint64_t start_brk = 0x603000;
+
+    uint64_t arg_start = 0x7fffffff0000;
+    uint64_t arg_end = 0x7fffffff1000;
+    uint64_t env_start = 0x7fffffff2000;
+    uint64_t env_end = 0x7fffffff3000;
+
+    int32_t exit_code = 0;
+
+    char statOutput[4096] = {0};
+    int  len = snprintf(
+        statOutput, 4096,
+        "%d (%s) %c %d %d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld "
+         "%ld %ld %ld %ld %lu %lu %ld %lu %lu %lu %lu %lu %lu %lu %lu "
+         "%lu %lu %lu %lu %lu %d %d %u %u %lu %lu %ld %lu %lu %lu %lu %lu %lu "
+         "%lu %d",
+        pid, comm, state, ppid, pgrp, session, tty_nr, tpgid, flags, minflt,
+        cminflt, majflt, cmajflt, utime, stime, cutime, cstime, priority, nice,
+        num_threads, itrealvalue, starttime, vsize, rss, rsslim, startcode,
+        endcode, startstack, kstkesp, kstkeip, signal, blocked, sigignore,
+        sigcatch, wchan, nswap, cnswap, exit_signal, processor, rt_priority,
+        policy, delayacct_blkio_ticks, guest_time, cguest_time, start_data,
+        end_data, start_brk, arg_start, arg_end, env_start, env_end, exit_code);
+    size_t toCopy = MIN(len - fd->pointer, limit);
+    memcpy(out, &statOutput[fd->pointer], toCopy);
+    fd->pointer += toCopy;
+    ret = toCopy;
+    break;
+  }
+  }
+
+  return ret;
 }
 
 size_t procEachOpen(char *filename, int flags, int mode, OpenFile *fd,
@@ -86,18 +194,50 @@ size_t procEachOpen(char *filename, int flags, int mode, OpenFile *fd,
     }
   }
   int pid = atoi(pidStr);
-  if (!strEql(decisionStr, "cmdline")) {
+  int opcode = 0;
+  if (strEql(decisionStr, "cmdline")) {
+    opcode = PROC_INTERNAL_CMDLINE;
+  } else if (strEql(decisionStr, "stat")) {
+    opcode = PROC_INTERNAL_STAT;
+  } else {
     free(badFn);
     return ERR(ENOENT);
   }
   free(badFn);
 
-  fd->dir = (void *)(size_t)pid;
+  UserspaceProc *uproc = calloc(sizeof(UserspaceProc), 1);
+  uproc->pid = pid;
+  uproc->opcode = opcode;
+  uproc->timesOpened = 1;
+  fd->dir = uproc;
   return 0;
 }
 
-VfsHandlers handleProcEach = {
-    .read = sRead, .stat = fakefsFstat, .open = procEachOpen};
+bool procEachDuplicate(OpenFile *original, OpenFile *orphan) {
+  orphan->dir = original->dir;
+  UserspaceProc *uproc = original->dir;
+  spinlockAcquire(&uproc->LOCK_PROP);
+  uproc->timesOpened++;
+  spinlockRelease(&uproc->LOCK_PROP);
+  return true;
+}
+
+bool procEachClose(OpenFile *fd) {
+  UserspaceProc *uproc = fd->dir;
+  spinlockAcquire(&uproc->LOCK_PROP);
+  uproc->timesOpened--;
+  if (!uproc->timesOpened)
+    free(uproc);
+  else
+    spinlockRelease(&uproc->LOCK_PROP);
+  return true;
+}
+
+VfsHandlers handleProcEach = {.read = sRead,
+                              .stat = fakefsFstat,
+                              .open = procEachOpen,
+                              .duplicate = procEachDuplicate,
+                              .close = procEachClose};
 
 void procSetup() {
   fakefsAddFile(&rootProc, rootProc.rootFile, "meminfo", 0,
