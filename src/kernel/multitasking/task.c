10266fc68f931d01a9195f1bcaba4de4dda19bf9
Panagiotis <contact@malwarepad.com>
2025-05-01 17:39:26 +0300

    Fix horrible race condition where malloc lock runs without interrupts!

--- src/kernel/multitasking/task.c
+++ src/kernel/multitasking/task.c
@@ -48,6 +48,8 @@ void taskAttachDefTermios(Task *task) {
 // although there are locks on these two functions, they are EXTREMELY unsafe!
 Task *taskListAllocate() {
   spinlockCntWriteAcquire(&TASK_LL_MODIFY);
+  Task *target = (Task *)malloc(sizeof(Task));
+  memset(target, 0, sizeof(Task)); // TASK_STATE_DEAD is 0 too
   asm volatile("cli");
   Task *browse = firstTask;
   while (browse) {
@@ -55,12 +57,9 @@ Task *taskListAllocate() {
       break; // found final
     browse = browse->next;
   }
+
   assert(browse);
-
-  Task *target = (Task *)malloc(sizeof(Task));
-  memset(target, 0, sizeof(Task)); // TASK_STATE_DEAD is 0 too
   browse->next = target;
-
   asm volatile("sti");
   spinlockCntWriteRelease(&TASK_LL_MODIFY);
   return target;
