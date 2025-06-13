c968b6994da61c1abcb5366ffc6b58d1675b8bfe
Panagiotis <contact@malwarepad.com>
2025-06-13 11:17:44 +0300

    Pass ecx into cpuid too for extended cpu features

--- src/kernel/cpu/system.c
+++ src/kernel/cpu/system.c
@@ -7,7 +7,7 @@
 void cpuid(uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx) {
   asm volatile("cpuid \n"
                : "=a"(*eax), "=b"(*ebx), "=c"(*ecx), "=d"(*edx)
-               : "a"(*eax)
+               : "a"(*eax), "c"(*ecx)
                : "memory");
 }
 
